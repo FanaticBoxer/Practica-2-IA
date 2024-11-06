1578b36cfa6798e1fdbc88cd7d03a51845fb4783
Carlos <carlos.calero.2525@gmail.com>
2024-11-06 14:24:56 +0100

    funcion nueva casillasAtacadas. Ahora ColocarCaballo pone las casillas que ataca el caballo a 1.

--- practica2IA.py
+++ practica2IA.py
@@ -1,30 +1,59 @@
-#import numpy as np
+#
+#DIMENSIONES DEL TABLERO
+N=8
+M=8
+
+    #DEVUELVE LISTA DE TOUPLES CON CASILLAS ATACADAS
+def casillasAtacadas(tablero, fila, columna):
+    atacadas=[]
+    ataques = ((1,2),(2,1),(2,-1),(1,-2),(-1,-2),(-2,-1),(-2,1),(-1,2))
+    
+    for atq in ataques:
+        ataqueFila = fila + atq[0]
+        ataqueColumna = columna + atq[1]
+    
+        if 0 <= ataqueFila < N and 0 <= ataqueColumna < M:
+            atacadas.append((ataqueFila, ataqueColumna))
+    
+    return atacadas
+
+
+
 
     #COLOCAR CABALLOS
 def colocarCaballo(tablero, fila, columna):
     if (0 <= fila < len(tablero) and 0 <= columna < len(tablero[0])):
         tablero[fila][columna]='k'
-        list [(1,2),(2,1),(2,-1),(1,-2)(-1,-2),(-2,-1)(-2,1)(-1,2)]
-    else: print ("Fuera de los limites")
+        
+    atacadas = casillasAtacadas(tablero, fila, columna)
+    for casilla in atacadas:
+        tablero[casilla[0]][casilla[1]] = 1
+
+        
+        
+    
+        
+
 
     
-    #COSTE DE UN CAMINO = CASILLAS LIBRES QUE ATACA EL NUEVO CABALLO
-    
+#COSTE DE UN CAMINO:
+#
+
+#def coste():
+
+
 def main():
-    
-    # DIMENSIONES DEL TABLERO
-    N = 8
-    M = 8
+
 
     # CREA EL TABLERO CON TODO 0s
     tablero = [[0 for _ in range(M)] for _ in range(N)]
 
     # Imprimir la matriz para verificar que está inicializada correctamente
-    for fila in tablero:
-        print(fila)
+    #for fila in tablero:
+    #    print(fila)
 
     colocarCaballo(tablero, 3, 3) 
-    print ("ESTADO NUMERO 1")
+    print ("ESTADO NUMERO 1\n\n")
     for fila in tablero:
         print(" ".join(f"{elem:>2}" for elem in fila))#(imprimir con formato)
         
